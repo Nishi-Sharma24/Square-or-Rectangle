b26413778e109f7ba45db22b57f69f83cdf7be4d
Nishi Sharma <84862478+Nishi-Sharma24@users.noreply.github.com>
2022-03-25 23:39:18 +0530

    Add files via upload

---
+++ rectangle  or square.py
@@ -0,0 +1,7 @@
+length=int(input("Enter the length:"))
+breath=int(input("Enter the breath:"))
+if (length==breath):
+    print("This is sqaure.")
+else:
+    print("This is rectangle.")
+input()    
